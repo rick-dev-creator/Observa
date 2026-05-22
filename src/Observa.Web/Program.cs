da0354697d4828f7761d6db337786fded7c9e4d4
rick-dev-creator <81363560+rick-dev-creator@users.noreply.github.com>
2026-05-22 19:35:22 +0900

    feat(solana): wire connector into solution, Web, and config

--- src/Observa.Web/Program.cs
+++ src/Observa.Web/Program.cs
@@ -5,6 +5,7 @@ using Observa.Components;
 using Observa.Connectors.Abstractions;
 using Observa.Connectors.Blofin;
 using Observa.Connectors.Patreon;
+using Observa.Connectors.Solana;
 using Observa.Features.Connectors.Catalog;
 using Observa.Features.Seed;
 using Observa.Features.Connectors.Manual;
@@ -49,6 +50,7 @@ builder.Services.AddSingleton<IConnector, ManualConnector>();
 builder.Services.AddSingleton<IConnector, RecurringConnector>();
 builder.Services.AddPatreonConnectors(builder.Configuration);
 builder.Services.AddBlofinConnectors(builder.Configuration);
+builder.Services.AddSolanaConnectors(builder.Configuration);
 builder.Services.AddSingleton<IConnectorRegistry, ConnectorRegistry>();
 builder.Services.AddSingleton<ConnectorPollOrchestrator>();
 
